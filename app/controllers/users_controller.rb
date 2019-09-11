a54866af8a47bf34b346a5bd0bd8db799a5c4ced
radchukd <radchuk.d@gmail.com>
2019-09-11 21:16:42 +0200

    add activity page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,10 +27,27 @@ class UsersController < ApplicationController
     @users = @users.sort { |u1, u2| u2.passive_relationships.count <=> u1.passive_relationships.count }
   end
 
+  def activity
+    @user  = current_user
+    @posts = Post.where(user_id: @user.id)
+    @posts = @posts.reject { |post| post.get_likes.size == 0 }
+    @likes  = []
+    @posts.each do |post|
+      post.get_likes.each do |like|
+        @likes << {
+          post: like.votable_id,
+          user: User.where(id: like.voter_id).first.nickname,
+          time: like.created_at
+        }
+      end
+    end
+    @likes = @likes.sort { |p1, p2| p2[:time] <=> p1[:time] }
+  end
+
   def search
     @users = User.where('nickname LIKE ?', "%#{params[:nickname]}%")
   end
-  
+
   private
 
   # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,12 @@ Rails.application.routes.draw do
 
   root to: 'feed#index'
   match 'users/:nickname' => 'users#show', 
-        :constraints => { :nickname => /[0-9A-Za-z\-\.]+/ },
+        :constraints => { :nickname => /[0-9A-Za-z\-\.\_]+/ },
         :via => [:get],
         :as => :user
   get 'users/:nickname/following' => 'users#following'
   get 'users/:nickname/followers' => 'users#followers'
   get 'discover' => 'users#discover'
+  get 'activity' => 'users#activity'
   get 'search'   => 'users#search'
 end
